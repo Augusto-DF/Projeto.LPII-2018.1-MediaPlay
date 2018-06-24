2ec295b4dccc509cc487c85e6db619f2c09f6e36
Thiago Monteiro <thig.dcm@hotmail.com>
2018-06-24 02:57:25 -0300

    Adding unfinished last method to Register, just need some info to finish it.

--- Media_Player/src/model/Register.java
+++ Media_Player/src/model/Register.java
@@ -1,6 +1,11 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.lang.Object;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import controllers.User;
 import controllers.Vip;
@@ -33,8 +38,7 @@ public class Register {
 	public String stringVip(Vip vip) {
 		String strUsr = "<"+vip.getId()+";"+vip.getNome()
 				+";"+vip.getSenha()+";"+vip.isVip()+";";
-
-		ArrayList<String> playlists = vip.getPlayLists();
+		ArrayList<String> playlists = vip.getPlaylists();
 		for(int i = 0; i < playlists.size(); ++i)
 		{
 			strUsr += playlists.get(i)+";";
@@ -47,16 +51,38 @@ public class Register {
 	
 	/**
 	 * Busca no arquivo usuario.txt um usuário passado por parametro e caso não exista no arquivo ele adiciona o novo usuário, antes
-	 * de registrar deve ser feita a verificação se o usuárioé ou não um usuário vip.
+	 * de registrar deve ser feita a verificação se o usuário é ou não um usuário vip.
 	 * @param user
 	 * @param filename
 	 */
-	public void registerUser(User user, String filename) {		
-		
+	public void registerUser(User user, String filename) throws IOException {		
+		try {
+	            File f = new File("src/usuario.txt");
+	            BufferedReader b = new BufferedReader(new FileReader(f));
+	            String readLine = "";
+	            System.out.println("Reading file using Buffered Reader");
+	            while ((readLine = b.readLine()) != null) {
+	            	b.skip('<'); b.skip('>');
+	            	String str = readLine;
+	            	List<String> userInfo = new ArrayList<String> ();
+	            	userInfo = Arrays.asList(str.split(";"));
+	           
+	                System.out.println(userInfo.get(1));
+	                System.out.println(userInfo.get(2));
+	                System.out.println(userInfo.get(3));
+	                System.out.println(userInfo.get(4));
+	            }
+	            b.close();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		    }
 		
 		
 	}
-}
+
 
 
 
